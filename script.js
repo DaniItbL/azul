04f6451dafc1a6bd61be0c9d96067bb5dd34dc5a
DaniItb <89824039+DaniItbL@users.noreply.github.com>
2025-06-26 22:28:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const gifts = [
         id: 2,
         title: "Regalo 2",
         type: "image",
-        content: "assets/images/Gbvqn7Db0AAKrFf.jpg" // Cambia por tu imagen
+        content: "azul/assets/images/Gbvqn7Db0AAKrFf.jpg" // Cambia por tu imagen
     },
     {
         id: 3,
